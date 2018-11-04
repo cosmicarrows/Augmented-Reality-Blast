a6d8e40cd4354cb00e2efba63565f2ba6f40b630
Laurence Wingo <laurencewingo@laurences-macbook-air.local>
2018-11-04 10:03:26 -0500

    Initial Commit

---
+++ AR Blast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR Blast
+//
+//  Created by Laurence Wingo on 11/4/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AR BlastTests/AR_BlastTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_BlastTests.swift
+//  AR BlastTests
+//
+//  Created by Laurence Wingo on 11/4/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import AR_Blast
+
+class AR_BlastTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
